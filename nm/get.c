9d285beae5b56baf8a157c4240050eed1a5839d5
valerian <valerian.huylebroeck@epitech.eu>
2018-02-21 19:17:27 +0100

    objdump starting

--- nm/get.c
+++ nm/get.c
@@ -10,10 +10,24 @@
 void get_str_tab(void)
 {
 	int i;
-	
+	char *name_s;
+
 	for (i = 0; i < nm.shnum; ++i) {
 		if (nm.shd[i].sh_type == SHT_STRTAB) {
 			nm.str_tab = &nm.shd[i];
+			name_s = (char*)nm.buf + nm.str_tab->sh_offset
+				+ nm.shd[i].sh_name;
+			if (!strcmp(name_s, ".shstrtab") && nm.osn == 0) {
+				nm.osn = nm.str_tab->sh_offset;
+				break;
+			}
+		}
+	}
+	for (i = 0; i < nm.shnum; ++i) {
+		name_s = (char*)nm.buf + nm.osn + nm.shd[i].sh_name;
+		if (!strcmp(name_s, ".strtab")) {
+			nm.str_tab = &nm.shd[i];
+			return;
 		}
 	}
 }
@@ -21,10 +35,11 @@ void get_str_tab(void)
 Elf64_Shdr get_section(int macro)
 {
 	int i;
-	
+
 	for (i = 0; i < nm.shnum; ++i) {
-		if (nm.shd[i].sh_type == macro) {
+		if (nm.shd[i].sh_type == (unsigned int)macro) {
 			return nm.shd[i];
 		}
 	}
+        return nm.shd[i];
 }
--- objdump/objdump_main.c
+++ objdump/objdump_main.c
@@ -16,6 +16,95 @@ int verif_flag(Elf64_Ehdr *elf)
 
 }
 
+typedef struct pc_s {
+	int macro;
+	char *str;
+} pc_t;
+
+static void get_machine_name()
+{
+  	int i = 0;
+
+	pc_t pc[20] = {{EM_NONE, "None"},
+		{EM_M32, "WE32100"},
+		{EM_SPARC, "Sparc"},
+		{EM_386, "Intel 80386"},
+		{EM_68K, "MC68000"},
+		{EM_PPC64, "PowerPC64"},
+		{EM_88K, "MC88000"},
+		{EM_PARISC, "HPPA"},
+		{EM_SPARC32PLUS, "Sparc v8+"},
+		{EM_PPC, "PowerPC"},
+		{EM_S390, "IBM S/390"},
+		{EM_MIPS, "MIPS R3000"},
+		{EM_ARM, "ARM"},
+		{EM_860, "Intel 80860"},
+		{EM_SH, "Renesas / SuperH SH"},
+		{EM_SPARCV9, "Sparc v9"},
+		{EM_IA_64, "Intel IA-64"},
+		{EM_X86_64, "i386:x86-64"}};
+	while (i < 18) {
+		if (pc[i].macro == objdump.elf->e_machine) {
+			objdump.machine_name = pc[i].str;
+			return ;
+		}
+		++i;
+	}
+	objdump.machine_name = "i386:x86-64";
+}
+
+void get_str_tab(void)
+{
+	int i;
+	
+	for (i = 0; i < objdump.shnum; ++i) {
+		if (objdump.shd[i].sh_type == SHT_STRTAB) {
+			objdump.str_tab = &objdump.shd[i];
+			return;
+		}
+	}
+}
+
+int print_shdr(const char *const fname, size_t size) {
+  int fd = open(fname, O_RDONLY);
+  //char *p = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
+	void *p = mmap(NULL, objdump.s.st_size, PROT_READ, MAP_PRIVATE, objdump.fd,
+			0);
+  Elf32_Ehdr *ehdr = (Elf32_Ehdr*)p;
+  Elf32_Shdr *shdr = (Elf32_Shdr *)(p + ehdr->e_shoff);
+  int shnum = objdump.elf->e_shnum;
+
+  Elf32_Shdr *sh_strtab = &shdr[ehdr->e_shstrndx];
+  const char *const sh_strtab_p = p + sh_strtab->sh_offset;
+int i;
+  for (i = 0; i < shnum; ++i) {
+    printf("%2d: %4d '%s'\n", i, shdr[i].sh_name,
+	   sh_strtab_p + shdr[i].sh_name);
+  }
+  return 0;
+}	
+
+void my_objdump(void)
+{
+	int i;
+
+	objdump.elf = objdump.buf;
+	objdump.shnum = objdump.elf->e_shnum;
+	objdump.shd = objdump.buf + objdump.elf->e_shoff;
+	get_machine_name();
+	get_str_tab();
+	//objdump.str_tab = &objdump.shdr[objdump.elf->e_shstrndx];
+	printf("\n%s:     file format %s\n", objdump.file_name, "elf64-x86-64");
+	printf("architecture: %s, flags 0x%08x:\n", objdump.machine_name,
+		objdump.elf->e_flags);
+	printf("start address 0x%016lx\n\n", objdump.elf->e_entry);
+	const char *const sh_strtab_p = objdump.buf + objdump.str_tab->sh_offset;
+	for (i = 0; i < objdump.shnum; ++i) {
+		printf("%i\n", (int)objdump.shd[i].sh_name);
+		printf("%s\n", (char*)(sh_strtab_p + objdump.shd[i].sh_name));
+	}
+}
+
 int start()
 {
 	if (objdump.fd != -1) {
@@ -26,8 +115,7 @@ int start()
 		objdump.buf = mmap(NULL, objdump.s.st_size, PROT_READ, MAP_PRIVATE, objdump.fd,
 			0);
 		if (objdump.buf != NULL && verif_flag((Elf64_Ehdr *)objdump.buf)) {
-			//my_objdump();
-			printf("obj\n");
+			my_objdump();
 		} else {
 			dprintf(2, "my_objdump: « %s »: not a valid file.\n",
 				objdump.file_name);
